bd1cfa0bacee482518aa0f7f493fbb07f1c83eff
Lukas Schnelle <lschnelle@asta.rwth-aachen.de>
2022-10-27 13:38:24 +0200

    add assignment 1

---
+++ assignment01/assignment.cc
@@ -0,0 +1,16 @@
+/*===========================================================================*/
+/*                                                                           *
+ * Copyright (c) 2020, Computer Graphics Group RWTH Aachen University        *
+ *                            All rights reserved                            *
+ *                                                                           *
+ * Basic Techniques in Computer Graphics Exercise                            *
+ *                                                                           */
+/*===========================================================================*/
+
+#include "assignment.hh"
+
+void task::drawScene(int scene, float runTime) {}
+
+void task::initCustomResources() {}
+
+void task::deleteCustomResources() {}
