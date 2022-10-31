37ae3ff53bab8a9ab7d0b216b4923e4e5c955f72
Lukas Schnelle <lschnelle@asta.rwth-aachen.de>
2022-10-31 16:31:06 +0100

    add assignment2

---
+++ assignment02/assignment.cc
@@ -0,0 +1,14 @@
+/*===========================================================================*/
+/*                                                                           *
+ * Copyright (c) 2020, Computer Graphics Group RWTH Aachen University        *
+ *                            All rights reserved                            *
+ *                                                                           *
+ * Basic Techniques in Computer Graphics Exercise                            *
+ *                                                                           */
+/*===========================================================================*/
+
+#include "assignment.hh"
+
+void task::drawScene(int scene, float runTime) { drawCircle(glm::vec3(1.0f, 0.0f, 0.0f), glm::mat4(1.)); }
+void task::initCustomResources() {}
+void task::deleteCustomResources() {}
